dc83aa2f2a0d71a8427f649b9e22050ce8d5bca0
Davide Corsi <davide.corsi@par-tec.it>
2021-08-09 16:38:46 +0200

    risoluzione

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,30 @@ public class Main {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Inserire la lunghezza della sequenza ");
 		int l = input.nextInt();
+		
+		// inizializzazione array
+		int[] nArray = new int[l];
+		
+		// lettura numeri
+		for(int i = 0; i < nArray.length; i++) {
+			System.out.print("Inserisci un numero ");
+			nArray[i] = input.nextInt();
+		}
+				
+		// ordinamento array
+		for(int i = 0; i < nArray.length -1; i++) {
+			for(int j = i + 1; j < nArray.length; j++) {
+				if(nArray[j] < nArray[i]) {
+					int n = nArray[j];
+					nArray[j] = nArray[i];
+					nArray[i] = n;
+				}
+			}
+		}
+		
+		// stampa del risultato
+		for(int n: nArray) {
+			System.out.println(n);
+		}
 	}
 }
