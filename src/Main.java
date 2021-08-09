0de789093697d363274fce15fdd4961b74fda4e2
Davide Corsi <davide.corsi@par-tec.it>
2021-08-09 16:37:48 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/*
+ Scrivete un applicazione che dato in ingresso una sequenza di numeri interi produca in output
+ la sequenza stessa in ordine crescente. Anche la lunghezza della sequenza deve essere stabilita dall'utente
+ */
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Inserire la lunghezza della sequenza ");
+		int l = input.nextInt();
+	}
+}
